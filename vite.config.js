52eded6f2100fa3bc4f14e95d3b09f547b85763f
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-06-25 11:02:07 -0600

    use dist

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
+
   plugins: [vue()],
   build: {
-    outDir: 'docs',
     sourcemap: false
   },
   server: {
